94f199485131ad49d63c901a7ba8726ccf388fc3
udykas <udykas@gmail.com>
2021-01-05 15:56:45 -0500

    Added styles and the title and description to Single Item page. Also updated Head to include the name of the item

--- components/SingleItem.js
+++ components/SingleItem.js
@@ -3,6 +3,7 @@ import Error from './ErrorMessage';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 const SingleItemStyles = styled.div`
   max-width: 1200px;
@@ -12,6 +13,15 @@ const SingleItemStyles = styled.div`
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
   min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
@@ -42,7 +52,14 @@ class SingleItem extends Component {
           const item = data.item;
           return( 
             <SingleItemStyles>
+              <Head>
+                <title>Fresh Fits | {item.title}</title>
+              </Head>
               <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2> Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
             </SingleItemStyles>
           )
         }}
