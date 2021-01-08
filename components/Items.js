897222774b2b0a96f8439e4a188c8dadb9e9477b
udykas <udykas@gmail.com>
2021-01-07 21:01:39 -0500

    Updated All Items Query to include new parameters created in backend schema

--- components/Items.js
+++ components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import perPage from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
